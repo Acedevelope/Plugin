e4a2407fddd0f153c41cd0fc306a34865459cf10
Acedevelope <nofacecoding@gmail.com>
2025-10-08 18:25:25 +0200

    delet all

--- Plugins/DialogueSystem/Source/DialogueSystem/Private/Interfaces/CharacterExpressionInterface.cpp
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Interfaces/CharacterExpressionInterface.h"
-
-
-// Add default functionality here for any ICharacterExpressionInterface functions that are not pure virtual.
